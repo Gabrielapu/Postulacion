ca9b10974d8843f831e684f877ae6fc478d7ddfb
Gabrielapu <gabrielperalta95@gmail.com>
2020-08-20 18:36:34 -0400

    BugFix

--- src/test/BurgerList.spec.js
+++ src/test/BurgerList.spec.js
@@ -2,6 +2,7 @@ import { shallowMount } from '@vue/test-utils'
 import BurgerList from '../components/BurgerList'
 
 // Prueba hacia el arreglo burgers, cuando esta vacio al no haber hamburguesas y al estar con hamburguesas.
+// No logre ejecutar el comando para hacer funcionar la prueba. Pero me guie por un articulo de internet.
 describe('BurgerList', () => {
     it('should render to a snapshot when burger list is empty', () => {
         const propsData = { burgers: [] }
